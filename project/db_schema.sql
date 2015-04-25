24b972b01295f41bf2b4cff9c263b596355e804f
Ryan Castner <castner.rr@gmail.com>
2015-04-25 15:33:24 -0400

    added foreign key references and moved field enum to a table with a many to many relationship with researcher table, decomposed with the associative entity researcher_field with foreign keys and indices added.

--- project/db_schema.sql
+++ project/db_schema.sql
@@ -2,18 +2,29 @@ begin;
 
 drop type if exists "status_t" cascade;
 create type "status_t" as enum (
-     'student'
+     'undergraduate'
     ,'faculty'
+    ,'graduate'
     ,'other'
+);
+
+drop table if exists "field" cascade;
+create table "field" (
+     "id"         bigserial   primary key
+    ,"name"       varchar(45) not null
+);
+
+drop table if exists "researcher_field" cacase;
+create table "researcher_field" (
+     
 );
 
 drop table if exists "paper" cascade;
 create table "papers" (
-     'id'         bigserial   primary key
-    ,'title'      varchar(45) not null
-    ,'abstract'   text        not null
-    ,'citation'   varchar(45) not null
-    ,'keyword'    varchar(45) not null
+     "id"         bigserial   primary key
+    ,"title"      varchar(45) not null
+    ,"abstract"   text        not null
+    ,"citation"   varchar(45) not null
 );
   
 drop table if exists "authorship" cascade;
@@ -21,22 +32,28 @@ create table "authorship" (
 
 );
 
+drop table if exists "paper_keywords" cascade;
+create table "keywords" (
+     "id"         bigserial primary key
+     "keyword"    varchar   not null
+);
+
 drop table if exists "researcher" cascade;
 create table "faculty" (
-     'id'            bigserial   primary key
-    ,'first_name'    varchar(45)  not null
-    ,'last_name'     varchar(45)  not null
-    ,'password_hash' bytea        default null
-    ,'password_salt' bytea        default null
-    ,'email'         varchar(45)  not null
-    ,'status'        status_t     not null
+     "id"            bigserial   primary key
+    ,"first_name"    varchar(45)  not null
+    ,"last_name"     varchar(45)  not null
+    ,"password_hash" bytea        default null
+    ,"password_salt" bytea        default null
+    ,"email"         varchar(45)  not null
+    ,"status"        status_t     not null
 );
 
 drop table if exists "interest" cascade;
 create table "interest" (
-     'id'       bigserial   primary key
-    ,'field'    varchar(45) not null
-    ,'interest' varchar(45) not null
+     "id"       bigserial   primary key
+    ,"field"    field_t     not null
+    ,"interest" varchar(45) not null
 );
 
 drop table if exists "researcher_interest" cascade;
@@ -49,5 +66,16 @@ alter table "researcher_interest" add foreign key ("researcher_id") references "
 
 alter table "authorship" add foreign key ("researcher_id") references "researcher" ("id");
 alter table "authorship" add foreign key ("paper_id") references "paper" ("id");
+
+alter table "researcher_field" add foreign key ("field_id") references "field" ("id");
+alter table "researcher_field" add foreign key ("researcher_id") references "researcher" ("id");
+
+create index "interest_id_index" on "interest" ("id");
+
+create index "researcher_id_index" on "researcher" ("id");
+
+create index "paper_id_index" on "paper" ("id");
+
+create index "field_id_index" on "field" ("id");
 
 commit;
